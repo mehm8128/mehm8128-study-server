ac91780e8abc3850fcaedeb07482d6dce3a161da
mehm8128 <mehm8128@gmail.com>
2022-08-08 23:36:12 +0900

    make quiz endpoint

--- router/memorize.go
+++ router/memorize.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"math/rand"
 	"mehm8128_study_server/model"
 	"net/http"
 
@@ -14,6 +15,10 @@ type Memorize struct {
 type Word struct {
 	Word   string `json:"word" db:"word"`
 	WordJp string `json:"wordJp" db:"word_jp"`
+}
+type QuizResponse struct {
+	Answer     model.WordResponse   `json:"answer" db:"answer"`
+	Selections []model.WordResponse `json:"selection" db:"selection"`
 }
 
 func getMemorizes(c echo.Context) error {
@@ -87,4 +92,48 @@ func postWord(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
 	return echo.NewHTTPError(http.StatusOK, res)
+}
+
+func getQuiz(c echo.Context) error {
+	ID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid id")
+	}
+	ctx := c.Request().Context()
+	words, err := model.GetWords(ctx, ID)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	if words == nil || len(words) < 4 {
+		return echo.NewHTTPError(http.StatusBadRequest, "cannot make quiz")
+	}
+	//配列をシャッフル、words配列の中身は答えとなる単語
+	for i := 0; i < len(words); i++ {
+		j := rand.Intn(len(words))
+		words[i], words[j] = words[j], words[i]
+	}
+	var quiz []*QuizResponse
+	for i := 0; i < len(words); i++ {
+		//答えと選択肢、選択肢どうしが重複しないように選択肢を作る
+		var numbers []int
+		for j := 0; j < 4; j++ {
+			numbers = append(numbers, rand.Intn(len(words)))
+			//todo:選択肢どうしで重複しないようにする
+		}
+		for j := 0; j < len(numbers); j++ {
+			if numbers[j] == i {
+				numbers[j] = (numbers[j] + 1) % len(words)
+			}
+		}
+		quiz = append(quiz, &QuizResponse{
+			Answer:     words[i],
+			Selections: []model.WordResponse{words[numbers[0]], words[numbers[1]], words[numbers[2]], words[numbers[3]]},
+		})
+		//答えの選択肢だけすり替え
+		answerNumber := rand.Intn(4)
+		quiz[i].Selections[answerNumber] = words[i]
+	}
+
+	return echo.NewHTTPError(http.StatusOK, quiz)
 }
