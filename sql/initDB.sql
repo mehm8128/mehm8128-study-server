13b9e5e0ff2cb47c060365c0ca82cc7957207ace
mehm8128 <mehm8128@gmail.com>
2022-08-23 23:15:23 +0900

    fix initDB.sql

--- sql/initDB.sql
+++ sql/initDB.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS "records" (
   "time" decimal(3) DEFAULT 0,
   "comment" varchar(140) DEFAULT '',
   "favorite_num" decimal(40) DEFAULT 0,
-  "file_id" char(36) NOT NULL,
+  "file_id" char(36),
   "created_by" char(36) NOT NULL,
   "created_at" timestamp NOT NULL,
   "updated_at" timestamp NOT NUll,
